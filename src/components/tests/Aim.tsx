7b38d7a72e43bd7b8b2a9dba31193671f1b35308
Illia Komarskyi <102596775+ottakist@users.noreply.github.com>
2023-07-08 20:33:21 +0300

    fixing routing issue

--- src/components/tests/Aim.tsx
+++ src/components/tests/Aim.tsx
@@ -33,7 +33,7 @@ const Aim = ({ setGameStatus }: TestProps) => {
       }))
       void updateUserFields(
         user?.sub ?? '1',
-        'Aiming Test',
+        'Aim Test',
         [`${Math.floor((time.endTime - time.startTime) / 30)} ms`],
         388
       )
